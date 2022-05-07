e73f9b9c66d92a470a89e531e84be25787ab4ad0
Ellenszenves <fallouterdo@gmail.com>
2022-05-07 15:59:38 +0200

    commit

--- shop.py
+++ shop.py
@@ -37,6 +37,7 @@ def sql(query):
     list(valamike)
 
 def list(valami):
+    lista.delete(0, tk.END)
     for item in valami:
         lista.insert(0, item)
 
@@ -45,14 +46,28 @@ def clear():
     #Így kell meghívni egy másik scriptet.
     import another
 
+def alert_box(alert_text):
+    alert = tk.Tk()
+    alert.geometry('250x150')
+    alert_text = tk.Label(alert, text=alert_text, font=("Arial", 15))
+    alert_text.pack()
+    alert.eval('tk::PlaceWindow . center')
+    ok_button = tk.Button(alert, text="OK!", command=alert.destroy, width=10)
+    ok_button.pack()
+    alert.mainloop()
+
 def select():
     obj_name = lista.get(tk.ANCHOR).split(',')
-    print(obj_name[1].rstrip(')'))
-    if label_select['text'] == 'Products':
-        cursor = conn.cursor()
-        cursor.execute('Select * from Product.products where name = ' + str(obj_name[1]))
-        valamike = cursor.fetchall()
-        label_more["text"] = valamike
+    if not lista.get(tk.ANCHOR):
+        alert_text="Product not selected!"
+        alert_box(alert_text)
+    else:
+        print(obj_name[1].rstrip(')'))
+        if label_select['text'] == 'Products':
+            cursor = conn.cursor()
+            cursor.execute('Select * from Product.products where name = ' + str(obj_name[1]))
+            valamike = cursor.fetchall()
+            label_more["text"] = valamike
 
 #Az ablak definiálása
 window = tk.Tk()
@@ -60,13 +75,14 @@ window.title("DBStart Project")
 window.geometry("800x600")
 window.columnconfigure(0, minsize=100, pad=10)
 window.columnconfigure(1, minsize=300)
-window.columnconfigure(2, minsize=150)
+window.columnconfigure(2, minsize=10)
+window.columnconfigure(3, minsize=150)
 
 label_select = tk.Label(text="Üdv!")
 label_select.grid(column=1, row=0)
 
 label_more = tk.Label(text="Üdv!")
-label_more.grid(column=2, row=0)
+label_more.grid(column=3, row=0)
 
 label = tk.Label(text="DBStart Python")
 label.grid(column=0, row=0, sticky=tk.NS)
@@ -87,7 +103,18 @@ btn_clear.grid(column=0, row=4, sticky=tk.NS)
 btn_select = tk.Button(window, text="Select", command=select, width=20)
 btn_select.grid(column=0, row=5, sticky=tk.NS)
 
-lista = tk.Listbox(window, width=40, height=30)
-lista.grid(column=1, row=1, sticky=tk.NS, rowspan=30)
+list_scroll = tk.Scrollbar(window)
+list_scroll.grid(column=2, row=1, rowspan=30, sticky=tk.N+tk.S)
 
+lista = tk.Listbox(window, width=40, height=30, yscrollcommand = list_scroll.set)
+lista.grid(column=1, row=1, sticky=tk.NE, rowspan=30)
+list_scroll.config(command = lista.yview)
+
+window.eval('tk::PlaceWindow . center')
 window.mainloop()
+
+def login():
+    window = tk.Tk()
+    label_select = tk.Label(text="Üdv!")
+    label_select.grid(column=1, row=0)
+    window.mainloop()
