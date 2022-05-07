05cd03f1cb1d9b0e804d28d0d0f1d7cd393d4df3
Ellenszenves <fallouterdo@gmail.com>
2022-05-07 17:59:24 +0200

    commit

--- shop.py
+++ shop.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+from re import S
 import tkinter as tk
 from turtle import width
 import pyodbc
 
+username = 'teszt'
+password = 'teszt'
 conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
 'Server=DESKTOP-AQBCCTD;'
 'UID=teszt;'
@@ -10,8 +13,23 @@ conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
 'Database=techshop;'
 'Trusted_Connection=yes;'
 )
-
 #Funkciók
+def connecting():
+    alert = tk.Tk()
+    alert.geometry('250x150')
+    username_text = tk.Label(alert, text="Username: ", font=("Arial", 12))
+    username_text.grid(column=0, row=0)
+    password_text = tk.Label(alert, text="Password: ", font=("Arial", 12))
+    password_text.grid(column=0, row=1)
+    username_entry = tk.Entry(alert)
+    username_entry.grid(column=1, row=0)
+    password_entry = tk.Entry(alert)
+    password_entry.grid(column=1, row=1)
+    alert.eval('tk::PlaceWindow . center')
+    ok_button = tk.Button(alert, text="OK!", command=alert.destroy, width=10)
+    ok_button.grid(columnspan=2, row=2)
+    alert.mainloop()
+
 def city():
     query = 'location.city'
     sql(query)
@@ -26,7 +44,7 @@ def prod():
 
 def sql(query):
     cursor = conn.cursor()
-    cursor.execute('Select * from ' + str(query))
+    cursor.execute('Select name from ' + str(query))
     valamike = cursor.fetchall()
     if query == 'Product.products':
         label_select["text"] = 'Products'
@@ -39,7 +57,7 @@ def sql(query):
 def list(valami):
     lista.delete(0, tk.END)
     for item in valami:
-        lista.insert(0, item)
+        lista.insert(0, item[0].replace(',','').replace("'",'').replace('(','').replace(')',''))
 
 def clear():
     lista.delete(0, tk.END)
@@ -62,12 +80,25 @@ def select():
         alert_text="Product not selected!"
         alert_box(alert_text)
     else:
-        print(obj_name[1].rstrip(')'))
+        print(obj_name[0])
         if label_select['text'] == 'Products':
             cursor = conn.cursor()
-            cursor.execute('Select * from Product.products where name = ' + str(obj_name[1]))
+            cursor.execute("Select * from Product.products where name = '" + str(obj_name[0]) + "'")
             valamike = cursor.fetchall()
-            label_more["text"] = valamike
+            more_info(obj_name, valamike)
+
+def more_info(obj_name, input):
+    stock = ''.join(str(e) for e in input)
+    name_text = tk.Label(window, text="Name: ", font=("Arial", 12))
+    name_text.grid(column=3, row=0)
+    stock_text = tk.Label(window, text="Stock: ", font=("Arial", 12))
+    stock_text.grid(column=3, row=1)
+    name_entry = tk.Entry(window)
+    name_entry.insert(tk.END, obj_name[0].replace('(','').replace(')',''))
+    name_entry.grid(column=4, row=0)
+    stock_entry = tk.Entry(window)
+    stock_entry.insert(tk.END, stock.split(',')[4].replace(')',''))
+    stock_entry.grid(column=4, row=1)
 
 #Az ablak definiálása
 window = tk.Tk()
@@ -80,9 +111,6 @@ window.columnconfigure(3, minsize=150)
 
 label_select = tk.Label(text="Üdv!")
 label_select.grid(column=1, row=0)
-
-label_more = tk.Label(text="Üdv!")
-label_more.grid(column=3, row=0)
 
 label = tk.Label(text="DBStart Python")
 label.grid(column=0, row=0, sticky=tk.NS)
@@ -102,6 +130,9 @@ btn_clear.grid(column=0, row=4, sticky=tk.NS)
 
 btn_select = tk.Button(window, text="Select", command=select, width=20)
 btn_select.grid(column=0, row=5, sticky=tk.NS)
+
+btn_connect = tk.Button(window, text="Connect", command=connecting, width=20)
+btn_connect.grid(column=0, row=6, sticky=tk.NS)
 
 list_scroll = tk.Scrollbar(window)
 list_scroll.grid(column=2, row=1, rowspan=30, sticky=tk.N+tk.S)
